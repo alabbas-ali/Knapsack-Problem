4e358a48b6114619aa5fab5a78955468077a9cbc
abass ali <weeamha@gmail.com>
2018-04-24 11:52:56 +0200

    factovisores problem

---
+++ src/Week2/Factovisors.java
@@ -0,0 +1,60 @@
+package Week2;
+
+import java.util.Scanner;
+
+public class Factovisors {
+
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        next: while (sc.hasNext()) {
+            int n = sc.nextInt(); //n!
+            int m = sc.nextInt(); // candidate m
+            // non-negative -> 0 divides nothing
+            if (m == 0) { 
+            	System.out.println(m + " does not divide " + n + "!");
+                continue;
+            }
+            if (m == 1) { // non-negative -> 1 divides everything
+            	System.out.println(m + " divides " + n + "!");
+                continue;
+            }
+            if(m <= n) {
+            	System.out.println(m + " divides " + n + "!");
+                continue;
+            }
+            int candidate = m; // m is mutable, so keep copy
+            int count = 0; // factor count
+            // Find factors of candidate up to sqrt(m)
+            for (int factor = 2; factor <= (int) Math.ceil(Math.sqrt(m)); factor++) {
+                // Count number of factors in m
+                while (m % factor == 0) {
+                    m /= factor;
+                    count++;
+                }
+                // If factors found
+                if (count > 0) {
+
+                    // Check if n! contains those factors
+                    for (int j = factor; j <= n; j *= factor) {
+                        count -= n / j;
+                    }
+                    // If n! does not contain all the factors, it m can't possibly divide n!
+                    if (count > 0) {
+                    	System.out.println(candidate + " does not divide " + n + "!");
+                        continue next; // continue with next test case
+                    }
+                }
+                count = 0; // reset factor count
+            }
+            // only if m <= n can m divide n!
+            if (m <= n) {
+            	System.out.println(candidate + " divides " + n + "!");
+            } else {
+            	System.out.println(candidate + " does not divide " + n + "!");
+
+            }
+        }
+
+        sc.close();
+    }
+}
--- src/Week2/PrimePath.java
+++ src/Week2/PrimePath.java
@@ -1,123 +1,112 @@
 package Week2;
 
-
+import java.io.BufferedInputStream;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class PrimePath {
-	static boolean[] isPrime = getPrimes(10000);
+class PrimePath {
 
-	private static boolean[] getPrimes(int n) {
+    static boolean[] isPrime = getPrimes(10000);
+
+    public static boolean[] getPrimes(int n) {
+        int i, j, k, x;
         boolean[] a = new boolean[n];
-        a[0] = false;
-        a[1] = false;
+        n++;
+        n /= 2;
+        int[] b = new int[(n + 1) * 2];
         a[2] = true;
         a[3] = true;
-        for (int i = 3; i <= n; i += 3) {
-        	 a[i] = checkPrime(i);
+        for (i = 1; i <= 2 * n; i++) {
+            b[i] = 0;
+        }
+        for (i = 3; i <= n; i += 3) {
+            for (j = 0; j < 2; j++) {
+                x = 2 * (i + j) - 1;
+                while (b[x] == 0) {
+                    a[x] = true;
+                    for (k = x; k <= 2 * n; k += x) {
+                        b[k] = 1;
+                    }
+                }
+            }
         }
         return a;
     }
-    
-    private static boolean checkPrime(int number) {
-		if (number % 2 == 0)
-			return false;
-		int root = (int) Math.sqrt((double) number);
-		for (int i = 3; i <= root; i += 2) {
-			if (number % i == 0)
-				return false;
-		}
-		return true;
-	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        int start;
-        int end;
-        int n = 0;
-        while (
-			sc.hasNextInt() && 
-			(start = sc.nextInt()) >= 1 && 
-			(end = sc.nextInt()) >= 1 && 
-			n++ <= 50
-		) {
-			
-			boolean found = false;
-			boolean[] isVisited = new boolean[10000];
-			int[] step = new int[10000];
-			LinkedList<Integer> queue = new LinkedList<>();
-			queue.addLast(start);
-			isVisited[start] = true;
-			int num = 0;
-			while (
-				!queue.isEmpty()
-			) {
-				int current = queue.pop();
-				num ++;
-				if (current == end) {
-					found = true;
-					break;
-				}
-				for (int j = 0; j <= 9; j++) {
-					int next1 = getNext(1, j, current);
-					int next2 = getNext(2, j, current);
-					int next3 = getNext(3, j, current);
-					int next4 = getNext(4, j, current);
-					
-					if (!isVisited[next1] && !isPrime[next1] && next1 <= 10000) {
-						queue.addLast(next1);
-						step[next1] = step[current] + 1;
-						isVisited[next1] = true;
-					}
-					if (!isVisited[next2] && !isPrime[next2] && next1 <= 10000) {
-						queue.addLast(next2);
-						step[next2] = step[current] + 1;
-						isVisited[next2] = true;
-					}
-					if (!isVisited[next3] && !isPrime[next3] && next1 <= 10000) {
-						queue.addLast(next3);
-						step[next3] = step[current] + 1;
-						isVisited[next3] = true;
-					}
-					if (!isVisited[next4] && !isPrime[next4] && next1 <= 10000) {
-						queue.addLast(next4);
-						step[next4] = step[current] + 1;
-						isVisited[next4] = true;
-					}
-				}
-			}
-			
-			if(found) {
-				System.out.println("Case " + n + ": " + num);
-			} else {
-				System.out.println("Case " + n + ": impossible");
-			}
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(new BufferedInputStream(System.in));
+        int cas = scan.nextInt();
+        for (int i = 1; i <= cas; i++) {
+            int start = scan.nextInt();
+            int end = scan.nextInt();
+            boolean[] isVisited = new boolean[10000];
+            int[] step = new int[10000];
+            LinkedList<Integer> queue = new LinkedList<>();
+            queue.addLast(start);
+            isVisited[start] = true;
+            while (!queue.isEmpty()) {
+                int current = queue.pop();
+                if (current == end) {
+                    break;
+                }
+                for (int j = 0; j <= 9; j++) {
+                    int next1 = getNext(1, j, current);
+                    int next2 = getNext(2, j, current);
+                    int next3 = getNext(3, j, current);
+                    int next4 = getNext(4, j, current);
+                    if (!isVisited[next1]) {
+                        queue.addLast(next1);
+                        step[next1] = step[current] + 1;
+                        isVisited[next1] = true;
+                    }
+                    if (!isVisited[next2]) {
+                        queue.addLast(next2);
+                        step[next2] = step[current] + 1;
+                        isVisited[next2] = true;
+                    }
+                    if (!isVisited[next3]) {
+                        queue.addLast(next3);
+                        step[next3] = step[current] + 1;
+                        isVisited[next3] = true;
+                    }
+                    if (!isVisited[next4]) {
+                        queue.addLast(next4);
+                        step[next4] = step[current] + 1;
+                        isVisited[next4] = true;
+                    }
+                }
+            }
+            System.out.println(step[end]);
         }
-        sc.close();
-	}
+        scan.close();
+    }
 
-	public static int getNext(int flag, int i, int current) {
-		int next = 0;
-		if (flag == 1) {
-			if (i == 0) {
-				return current;
-			}
-			next = current % 1000 + i * 1000;
-		}
-		if (flag == 2) {
-			int t = current / 1000;
-			next = t * 1000 + current % 1000 % 100 + i * 100;
-		}
-		if (flag == 3) {
-			int t = current / 100;
-			int tt = current % 10;
-			next = t * 100 + i * 10 + tt;
-		}
-		if (flag == 4) {
-			next = current / 10 * 10 + i;
+    public static int getNext(int flag, int i, int current) {
+        int next = 0;
+        if (flag == 1) {
+            if (i == 0) {
+                return current;
+            }
+            next = current % 1000 + i * 1000;
+        }
+        if (flag == 2) {
+            int t = current / 1000;
+            next = t * 1000 + current % 1000 % 100 + i * 100;
+        }
+        if (flag == 3) {
+            int t = current / 100;
+            int tt = current % 10;
+            next = t * 100 + i * 10 + tt;
+        }
+        if (flag == 4) {
+            next = current / 10 * 10 + i;
 
-		}
-		return next;
-	}
+        }
+        if (!isPrime[next]) {
+            return current;
+        }
+        return next;
+    }
 
 }
